0daf995150e275bf3598dd3c731c8794fddb9073
dhslrl321 <dhslrl321@gmail.com>
2021-12-16 15:20:24 +0900

    #feature - Interaction 컴포넌트 추가 및 생성 API 연동 완료

--- pages/assignment/class/[id].js
+++ pages/assignment/class/[id].js
@@ -4,8 +4,8 @@ import CourseAssignment from "../../../src/section/course-assignment";
 import AdminCourseMain from "../../../src/section/admin-course-main";
 import axios from 'axios';
 
-export default function assignment ({nMe, nClassDetail, nStudentList, nQuizList}) {
-  return nMe.role=='강사' ? <div style={{ width: "100%" }}><AdminCourseMain nMe={nMe} nClassDetail={nClassDetail} nStudentList={nStudentList} nQuizList={nQuizList} /></div> : <CourseAssignment nMe={nMe} nClassDetail={nClassDetail} nQuizList={nQuizList} />
+export default function assignment({ nMe, nClassDetail, nStudentList, nQuizList, nInteractionList }) {
+  return nMe.role == '강사' ? <div style={{ width: "100%" }}><AdminCourseMain nMe={nMe} nClassDetail={nClassDetail} nStudentList={nStudentList} nQuizList={nQuizList} nInteractionList={nInteractionList} /></div> : <CourseAssignment nMe={nMe} nClassDetail={nClassDetail} nQuizList={nQuizList} />
 }
 
 export const getServerSideProps = async (ctx) => {
@@ -13,18 +13,19 @@ export const getServerSideProps = async (ctx) => {
   let nClassDetail = {};
   let nStudentList = [];
   let nQuizList = [];
+  let nInteractionList = [];
 
   // 이용자정보 조회
   try {
     const cookies = ctx.req.headers.cookie;
     nMe = await axios.get(`/v1/authenticate/me`, {
       headers: {
-          Cookie: cookies
+        Cookie: cookies
       },
-      withCredentials : true
+      withCredentials: true
     });
-  } catch(err) {
-    if(err?.response?.status == 403 || err?.response?.status == 401){
+  } catch (err) {
+    if (err?.response?.status == 403 || err?.response?.status == 401) {
       console.log('로그인 전');
     }
   }
@@ -34,12 +35,12 @@ export const getServerSideProps = async (ctx) => {
     const cookies = ctx.req.headers.cookie;
     nClassDetail = await axios.get(`/v1/users/${nMe.data?.id}/participating`, {
       headers: {
-          Cookie: cookies
+        Cookie: cookies
       },
-      withCredentials : true
+      withCredentials: true
     });
-  } catch(err) {
-    if(err?.response?.status == 403 || err?.response?.status == 401){
+  } catch (err) {
+    if (err?.response?.status == 403 || err?.response?.status == 401) {
       console.log('로그인 전');
     }
   }
@@ -49,12 +50,12 @@ export const getServerSideProps = async (ctx) => {
     const cookies = ctx.req.headers.cookie;
     nQuizList = await axios.get(`/v1/courses/${ctx.query.id}/assignments`, {
       headers: {
-          Cookie: cookies
+        Cookie: cookies
       },
-      withCredentials : true
+      withCredentials: true
     });
-  } catch(err) {
-    if(err?.response?.status == 403 || err?.response?.status == 401){
+  } catch (err) {
+    if (err?.response?.status == 403 || err?.response?.status == 401) {
       console.log('로그인 전');
     }
   }
@@ -64,12 +65,27 @@ export const getServerSideProps = async (ctx) => {
     const cookies = ctx.req.headers.cookie;
     nStudentList = await axios.get(`/v1/users/${nMe.data?.id}/courses/${ctx.query.id}`, {
       headers: {
-          Cookie: cookies
+        Cookie: cookies
       },
-      withCredentials : true
+      withCredentials: true
     });
-  } catch(err) {
-    if(err?.response?.status == 403 || err?.response?.status == 401){
+  } catch (err) {
+    if (err?.response?.status == 403 || err?.response?.status == 401) {
+      console.log('로그인 전');
+    }
+  }
+
+  // 인터렉션 조회
+  try {
+    const cookies = ctx.req.headers.cookie;
+    nInteractionList = await axios.get(`/v1/interactions/courses/${ctx.query.id}`, {
+      headers: {
+        Cookie: cookies
+      },
+      withCredentials: true
+    });
+  } catch (err) {
+    if (err?.response?.status == 403 || err?.response?.status == 401) {
       console.log('로그인 전');
     }
   }
@@ -77,9 +93,10 @@ export const getServerSideProps = async (ctx) => {
   return {
     props: {
       nMe: nMe?.data ? nMe?.data : {},
-      nClassDetail: nClassDetail.data?.course ? nClassDetail.data?.course.filter((item)=>item.courseId==ctx.query.id).map((item)=>{item.thumnailImageUrl="https://cdn.inflearn.com/public/courses/327762/cover/d37b231e-411f-4358-9b28-e3839f79f42b/327762-eng.png";return item;})[0] : {},
+      nClassDetail: nClassDetail.data?.course ? nClassDetail.data?.course.filter((item) => item.courseId == ctx.query.id).map((item) => { item.thumnailImageUrl = "https://cdn.inflearn.com/public/courses/327762/cover/d37b231e-411f-4358-9b28-e3839f79f42b/327762-eng.png"; return item; })[0] : {},
       nQuizList: nQuizList.data ? nQuizList.data : [],
-      nStudentList: nStudentList.data ? nStudentList.data.filter((item)=>nMe.data.id!=item.studentId) : []
+      nStudentList: nStudentList.data ? nStudentList.data.filter((item) => nMe.data.id != item.studentId) : [],
+      nInteractionList: nInteractionList.data ? nInteractionList.data : [],
     }
-}
+  }
 }
--- src/section/admin-course-main/index.js
+++ src/section/admin-course-main/index.js
@@ -37,7 +37,7 @@ import SuccessAlert from '../../component/SuccessAlert';
 
 
 const convertToChip = (string) => {
-  if (string === "마감") return <Chip label={string} />
+  if (string === "OX") return <Chip label="OX 퀴즈" color="success" />
   else if (string === "진행중" || string === "교육생") return <Chip color="success" label={string} />
 }
 
@@ -67,14 +67,17 @@ function a11yProps(index) {
   };
 }
 
-export default function CourseMain ({nMe, nClassDetail, nStudentList, nQuizList}) {
+export default function CourseMain({ nMe, nClassDetail, nStudentList, nQuizList, nInteractionList }) {
+
   const router = useRouter();
   const [assignments, setAssignments] = useState([]);
   const [invitation, setInvitation] = useState("")
   const [invitationModal, setInvitationModal] = useState(false);
   const [assignmentModal, setAssignmentModal] = useState(false);
+  const [interactionModal, setInteractionModal] = useState(false);
   const [containerStatus, setContainerStatus] = useState('중지');
   const [quizTitle, setQuizTitle] = useState('');
+
   const [quizDescription, setQuizDescription] = useState('');
   const [quizOpen, setQuizOpen] = useState(Date.now());
   const [quizClose, setQuizClose] = useState(Date.now());
@@ -85,26 +88,47 @@ export default function CourseMain ({nMe, nClassDetail, nStudentList, nQuizList}
     'score': 0
   }]);
 
+
+  const [interactionTitle, setInteractionTitle] = useState('');
+  const [interactionType, setInteractionType] = useState("");
+
   const handleQuiz = async () => {
     try {
       const nResult = await axios.post(`/v1/courses/${router.query.id}/assignments`, {
-        'title' : quizTitle,
-        'description' : quizDescription,
-        'startedAt' : quizOpen,
-        'endedAt' : quizClose
+        'title': quizTitle,
+        'description': quizDescription,
+        'startedAt': quizOpen,
+        'endedAt': quizClose
       });
       await axios.post(`/v1/courses/${router.query.id}/assignments/${nResult.data.assignmentId}`, quizChildrenList);
       //SuccessAlert('과제가 성공적으로 추가되었습니다.');
       router.reload();
-    } catch(err) {
+    } catch (err) {
       console.log('error :', err)
-      if(err?.response?.status == 403 || err?.response?.status == 401){
+      if (err?.response?.status == 403 || err?.response?.status == 401) {
         FailureAlert('로그인이 필요합니다.');
       } else {
         FailureAlert('에러가 발생하였습니다.');
       }
     }
     setAssignmentModal(false);
+  }
+
+  const handleInteractionSubmit = async () => {
+    try {
+      const nResult = await axios.post(`/v1/interactions/courses/${router.query.id}`, {
+        'title': interactionTitle,
+        'interactionType': interactionType,
+      });
+      router.reload();
+    } catch (err) {
+      console.log('error :', err)
+      if (err?.response?.status == 403 || err?.response?.status == 401) {
+        FailureAlert('로그인이 필요합니다.');
+      } else {
+        FailureAlert('에러가 발생하였습니다.');
+      }
+    }
   }
 
   const handleQuizChildrenDescription = (data, index) => {
@@ -116,7 +140,7 @@ export default function CourseMain ({nMe, nClassDetail, nStudentList, nQuizList}
       'score': quizChildrenList[index].score
     }
     setQuizChildrenList(copyList);
-  } 
+  }
 
   const handleQuizChildrenAnswer = (data, index) => {
     const copyList = quizChildrenList.slice();
@@ -127,7 +151,7 @@ export default function CourseMain ({nMe, nClassDetail, nStudentList, nQuizList}
       'score': quizChildrenList[index].score
     }
     setQuizChildrenList(copyList);
-  } 
+  }
 
   const handleQuizChildrenScore = (data, index) => {
     const copyList = quizChildrenList.slice();
@@ -138,7 +162,7 @@ export default function CourseMain ({nMe, nClassDetail, nStudentList, nQuizList}
       'score': parseInt(data)
     }
     setQuizChildrenList(copyList);
-  } 
+  }
 
   const addQuizChildren = () => {
     const copyList = quizChildrenList.slice();
@@ -168,6 +192,11 @@ export default function CourseMain ({nMe, nClassDetail, nStudentList, nQuizList}
   const handleModalClose = () => {
     setInvitationModal(false);
     setAssignmentModal(false);
+    setInteractionModal(false);
+  }
+
+  const handleInteractionModalOpen = () => {
+    setInteractionModal(true);
   }
 
   const [loading, setLoading] = useState(false);
@@ -196,6 +225,10 @@ export default function CourseMain ({nMe, nClassDetail, nStudentList, nQuizList}
     setTimeout(invitationLoadingFalse, 2000);
   }
 
+  const handleInteractionType = (event) => {
+    setInteractionType(event.target.value);
+  };
+
   const { courseInfo, students: { signedUp, register } } = data;
 
   const [value, setValue] = useState(0);
@@ -222,8 +255,8 @@ export default function CourseMain ({nMe, nClassDetail, nStudentList, nQuizList}
             <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
               <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
                 <Tab label="과제" {...a11yProps(0)} />
-                <Tab label="구성원 관리" {...a11yProps(1)} />
-                {/*<Tab label="클래스 관리" {...a11yProps(2)} />*/}
+                <Tab label="실시간 퀴즈" {...a11yProps(1)} />
+                <Tab label="구성원 관리" {...a11yProps(2)} />
               </Tabs>
             </Box>
             <TabPanel value={value} index={0}>
@@ -231,7 +264,7 @@ export default function CourseMain ({nMe, nClassDetail, nStudentList, nQuizList}
                 <WhiteBackground>
                   <S.AdminHeader>
                     <h3>과제</h3>
-                    <S.Text onClick={handleCreateAssignment}>생성하기</S.Text>
+                    <S.Text onClick={handleCreateAssignment}>과제 생성</S.Text>
                   </S.AdminHeader>
                   <Divider />
                   <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -255,7 +288,7 @@ export default function CourseMain ({nMe, nClassDetail, nStudentList, nQuizList}
                             {assignment.assignmentId}
                           </TableCell>
                           <TableCell align="left">{assignment.title}</TableCell>
-                          <TableCell align="right">{convertToChip(Date.now()-new Date(assignment.endedAt)<0 ? '진행중' : '마감')}</TableCell>
+                          <TableCell align="right">{convertToChip(Date.now() - new Date(assignment.endedAt) < 0 ? '진행중' : '마감')}</TableCell>
                           <TableCell align="right">{new Date(assignment.startedAt).toLocaleDateString()} ~ {new Date(assignment.endedAt).toLocaleDateString()}</TableCell>
                         </TableRow>
                       ))}
@@ -265,6 +298,48 @@ export default function CourseMain ({nMe, nClassDetail, nStudentList, nQuizList}
               </div>
             </TabPanel>
             <TabPanel value={value} index={1}>
+              <div style={{ marginTop: "-100px" }}>
+                <WhiteBackground>
+                  <S.AdminHeader>
+                    <h3>
+                      상호작용
+                    </h3>
+                    <S.Text style={{ cursor: "pointer" }} onClick={handleInteractionModalOpen}>
+                      상호작용 생성
+                    </S.Text>
+                  </S.AdminHeader>
+                  <Divider />
+                  <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>번호</TableCell>
+                        <TableCell align="left">상호작용 이름</TableCell>
+                        <TableCell align="right">타입</TableCell>
+                        <TableCell align="right">생성일</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {nInteractionList.length === 0 ? <div style={{ width: "100%", textAlign: "center" }}><h4>상호작용이 존재하지 않습니다.</h4></div> : nInteractionList.map((interaction) => (
+                        <TableRow
+                          key={interaction.interactionId}
+                          hover
+                          style={{ cursor: "pointer" }}
+                          sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                          <TableCell component="th" scope="row">
+                            {interaction.interactionId}
+                          </TableCell>
+                          <TableCell align="left">{interaction.title}</TableCell>
+                          <TableCell align="right">{convertToChip(interaction.interactionType)}</TableCell>
+                          <TableCell align="right">{new Date(interaction.createdAt).toLocaleDateString()}</TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </WhiteBackground>
+              </div>
+            </TabPanel>
+            <TabPanel value={value} index={2}>
               <div style={{ marginTop: "-100px" }}>
                 <WhiteBackground>
                   <S.AdminHeader>
@@ -289,7 +364,7 @@ export default function CourseMain ({nMe, nClassDetail, nStudentList, nQuizList}
                     <TableBody>
                       {nStudentList.length === 0 ? <div style={{ width: "100%", textAlign: "center" }}><h4>현재 수강중인 학생이 존재하지 않습니다.</h4></div> : nStudentList.map((student) => (
                         <TableRow
-                          onClick={()=>{router.push(`/ground/${router.query.id}`)}}
+                          onClick={() => { router.push(`/ground/${router.query.id}`) }}
                           key={student.studentId}
                           hover
                           style={{ cursor: "pointer" }}
@@ -337,9 +412,6 @@ export default function CourseMain ({nMe, nClassDetail, nStudentList, nQuizList}
                 </WhiteBackground>
               </div>
             </TabPanel>
-            <TabPanel value={value} index={2}>
-              Item Three
-      </TabPanel>
           </Box>
         </S.AssignmentWrapper>
       </S.Container>
@@ -349,30 +421,30 @@ export default function CourseMain ({nMe, nClassDetail, nStudentList, nQuizList}
           <S.InputColumn>
             <h3>과제명</h3>
             {/*<S.HelpText>해당 클래스에 타이틀로 사용될 이름입니다.</S.HelpText>*/}
-            <TextField label="과제명" variant="outlined" value={quizTitle} onChange={(e)=>{setQuizTitle(e.target.value)}} />
+            <TextField label="과제명" variant="outlined" value={quizTitle} onChange={(e) => { setQuizTitle(e.target.value) }} />
           </S.InputColumn>
 
           <S.InputColumn style={{ width: "80%" }}>
             <h3>과제 상세 설명</h3>
-            <TextField fullWidth label="과제 설명" variant="outlined" value={quizDescription} onChange={(e)=>{setQuizDescription(e.target.value)}} multiline rows={2} />
+            <TextField fullWidth label="과제 설명" variant="outlined" value={quizDescription} onChange={(e) => { setQuizDescription(e.target.value) }} multiline rows={2} />
           </S.InputColumn>
 
           <S.InputColumn>
             <h3>공개 및 마감 시간</h3>
             <div>
               <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <DateTimePicker
-                label="공개시간"
-                value={quizOpen}
-                onChange={(e)=>{setQuizOpen(new Date(e))}}
-                renderInput={(params) => <TextField style={{ paddingRight: "30px" }} {...params} />}
-              />
-              <DateTimePicker
-                label="마감시간"
-                value={quizClose}
-                onChange={(e)=>{setQuizClose(new Date(e))}}
-                renderInput={(params) => <TextField {...params} />}
-              />
+                <DateTimePicker
+                  label="공개시간"
+                  value={quizOpen}
+                  onChange={(e) => { setQuizOpen(new Date(e)) }}
+                  renderInput={(params) => <TextField style={{ paddingRight: "30px" }} {...params} />}
+                />
+                <DateTimePicker
+                  label="마감시간"
+                  value={quizClose}
+                  onChange={(e) => { setQuizClose(new Date(e)) }}
+                  renderInput={(params) => <TextField {...params} />}
+                />
               </LocalizationProvider>
             </div>
           </S.InputColumn>
@@ -381,20 +453,55 @@ export default function CourseMain ({nMe, nClassDetail, nStudentList, nQuizList}
             <h3>주관식 문제</h3>
             <S.HelpText>주관식 문제에 대한 설명과 배점을 입력하세요</S.HelpText>
             {
-              quizChildrenList.map((item, index)=>{
+              quizChildrenList.map((item, index) => {
                 return (
-                <div style={{ width: "100%", display: "flex", justifyContent: "flex-start", alignItems: "center", marginBottom: "5px" }}>
-                <TextField sx={{ width: "50%", marginRight: "20px" }} fullWidth label="문제" variant="outlined" value={item.description} onChange={(e)=>{handleQuizChildrenDescription(e.target.value, index)}} />
-                <TextField sx={{ width: "30%", marginRight: "20px" }} fullWidth label="정답" variant="outlined" value={item.answer} onChange={(e)=>{handleQuizChildrenAnswer(e.target.value, index)}} />
-                <TextField sx={{ width: "20%", marginRight: "20px" }} fullWidth label="배점" variant="outlined" value={item.score} onChange={(e)=>{handleQuizChildrenScore(e.target.value, index)}} />
-                {quizChildrenList.length == index+1 ? <AddCircleOutlineIcon onClick={addQuizChildren} sx={{ color: "green" }} /> : <RemoveCircleOutlineIcon onClick={()=>{deleteQuizChildren(index)}} sx={{ color: "red" }} />}
-                </div>
+                  <div style={{ width: "100%", display: "flex", justifyContent: "flex-start", alignItems: "center", marginBottom: "5px" }}>
+                    <TextField sx={{ width: "50%", marginRight: "20px" }} fullWidth label="문제" variant="outlined" value={item.description} onChange={(e) => { handleQuizChildrenDescription(e.target.value, index) }} />
+                    <TextField sx={{ width: "30%", marginRight: "20px" }} fullWidth label="정답" variant="outlined" value={item.answer} onChange={(e) => { handleQuizChildrenAnswer(e.target.value, index) }} />
+                    <TextField sx={{ width: "20%", marginRight: "20px" }} fullWidth label="배점" variant="outlined" value={item.score} onChange={(e) => { handleQuizChildrenScore(e.target.value, index) }} />
+                    {quizChildrenList.length == index + 1 ? <AddCircleOutlineIcon onClick={addQuizChildren} sx={{ color: "green" }} /> : <RemoveCircleOutlineIcon onClick={() => { deleteQuizChildren(index) }} sx={{ color: "red" }} />}
+                  </div>
                 )
               })
             }
           </S.InputColumn>
           <div style={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
             <S.Button onClick={handleQuiz}>
+              {loading ? <CircularProgress style={{ color: "white" }} /> : "생성하기"}
+            </S.Button>
+          </div>
+        </S.CreateAssignmentWrapper>
+      </Modal>
+      <Modal open={interactionModal} handleOnModalClose={handleModalClose}>
+        <S.CreateAssignmentWrapper>
+          <h3>인터렉션 생성하기</h3>
+
+          <S.InputColumn >
+            <h3>타입</h3>
+            <Box sx={{ minWidth: 220 }}>
+              <FormControl fullWidth>
+                <InputLabel fullWidth>상호작용 타입</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={interactionType}
+                  label="Type"
+                  onChange={handleInteractionType}
+                >
+                  <MenuItem value="OX">OX 퀴즈</MenuItem>
+                  <MenuItem value="TODO">TODO List</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+          </S.InputColumn>
+
+          <S.InputColumn>
+            <h3>인터렉션 이름</h3>
+            <TextField label="타이틀" variant="outlined" value={interactionTitle} onChange={(e) => { setInteractionTitle(e.target.value) }} />
+          </S.InputColumn>
+
+          <div style={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <S.Button onClick={handleInteractionSubmit}>
               {loading ? <CircularProgress style={{ color: "white" }} /> : "생성하기"}
             </S.Button>
           </div>
