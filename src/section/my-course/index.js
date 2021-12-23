b937dbcdbb8b937767f01b199a5d59e662c618b2
KoHyunsu <sce06147@naver.com>
2021-12-23 10:37:33 +0900

    [Fix] 클래스 생성 복구

--- src/section/my-course/index.js
+++ src/section/my-course/index.js
@@ -17,7 +17,7 @@ const MyCourse = ({ isAdmin, nClass, nEvaluation, nMe }) => {
   return (
     <S.Container>
       <WhiteBackground>
-        {nMe.role=='강사' ? <S.AdminHeader><S.Title></S.Title><div style={{ display: "flex" }}>{/*<Link href="/admin/create"><S.Text style={{ cursor: "pointer" }}>클래스 생성하기</S.Text></Link><Link href="/admin/create-test"><S.Text style={{ cursor: "pointer" }}>테스트 생성하기</S.Text></Link>*/}</div></S.AdminHeader> : <S.Title> 진행 중인 클래스 및 테스트</S.Title>}
+        {nMe.role=='강사' ? <S.AdminHeader><S.Title></S.Title><div style={{ display: "flex" }}><Link href="/admin/create"><S.Text style={{ cursor: "pointer" }}>클래스 생성하기</S.Text></Link><Link href="/admin/create-test"><S.Text style={{ cursor: "pointer" }}>테스트 생성하기</S.Text></Link></div></S.AdminHeader> : <S.Title> 진행 중인 클래스 및 테스트</S.Title>}
         <Divider />
         <S.CourseWrapper>
           <S.ClassWrapper>
@@ -29,12 +29,12 @@ const MyCourse = ({ isAdmin, nClass, nEvaluation, nMe }) => {
             </S.CardWrapper>
           </S.ClassWrapper>
           <S.TestWrapper>
-            {/* <S.Title>내가 참여하는 테스트</S.Title>
+            <S.Title>내가 참여하는 테스트</S.Title>
             <S.CardWrapper>
               {nEvaluation.map(test => <div key={test.evaluationId}>
                 <CourseCard spaceId={test.evaluationId} title={test.title} instructor={test.teacherName} thumnailImageUrl={test.thumnailImageUrl} checker={'test'} />
               </div>)}
-            </S.CardWrapper> */}
+            </S.CardWrapper>
           </S.TestWrapper>
         </S.CourseWrapper>
       </WhiteBackground>
